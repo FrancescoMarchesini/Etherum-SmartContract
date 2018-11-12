7f8dc435e5da54814c6eb3479f6e9f66746db6f1
Francesco <marchesinifrancesco1@gmail.com>
2018-11-12 20:07:42 +0100

    Create 01_hello_world.sol

---
+++ 01_hello_world.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.25;
+
+contract Hello{
+    string hello;
+    
+    constructor() public{
+        hello = "hello world";
+    }
+    
+    //
+    function getHello() public view returns(string){
+        return hello;
+    }
+}
